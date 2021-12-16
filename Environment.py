3d4b8a70c257e2a07bc107b7907fc76db233aeb5
Tom Tschiller <thomas.schmid.3@students.bfh.ch>
2021-12-16 11:00:12 +0100

    fix: add condition for finding gifts in area
    modify .gitignore for VSCode
    
    remove unused method
    
    add a more dynamic threshold to find nearest gifts
    
    add local optimization algorithm / refactor get_measure_method

--- Environment.py
+++ Environment.py
@@ -1,3 +1,4 @@
+import math
 import pandas as pd
 from TSPRoute import TSPRoute
 
@@ -30,11 +31,32 @@ class Environment:
         not_assigned = not_assigned.where(not_assigned['Weight'] <= space_in_slave)
         return not_assigned.dropna()
 
-    def find_gifts_in_area(self, gift: pd.Series, gifts_assigned_to_routes: pd.DataFrame, cur_tsp_route: TSPRoute, threshold: float) -> pd.DataFrame:
+    def find_gifts_in_area(self, gift: pd.Series, gifts_assigned_to_routes: pd.DataFrame, cur_tsp_route: TSPRoute) -> pd.DataFrame:
         gifts_not_assigned_yet = self.__get_gifts_not_assigned_yet(gifts_assigned_to_routes, cur_tsp_route)
-        gifts_not_assigned_yet = gifts_not_assigned_yet.where(gifts_not_assigned_yet["Longitude"] >= (gift["Longitude"] - threshold))
-        gifts_not_assigned_yet = gifts_not_assigned_yet.where(gifts_not_assigned_yet["Longitude"] <= (gift["Longitude"] + threshold))
-        gifts_not_assigned_yet = gifts_not_assigned_yet.where(gifts_not_assigned_yet["Latitude"] >= (gift["Latitude"] - threshold))
-        gifts_not_assigned_yet.where(gifts_not_assigned_yet["Latitude"] <= (gift["Latitude"] + threshold))
-        gifts_not_assigned_yet = gifts_not_assigned_yet.dropna()
+
+        coordinates_min = gifts_not_assigned_yet.min()
+        coordinates_max = gifts_not_assigned_yet.max()
+
+        # the initial threshold is set to the farest distance of all gifts not assigned yet. this is the bigger distance to either the min or the 
+        # max value of all gifts.
+        threshold_longitude: float = max(abs(coordinates_max["Longitude"] - gift["Longitude"]), abs(coordinates_min["Longitude"] + gift["Longitude"]))
+        threshold_latitude: float = max(abs(coordinates_max["Latitude"] - gift["Latitude"]), abs(coordinates_min["Latitude"] + gift["Latitude"]))
+
+        gifts_amount_target = 500
+        gifts_amount_max = gifts_amount_target * 2
+
+        # the threshold is decreased in each iteration to find only the closest gifts to the current gift. the loop aborts if the search has been 
+        # narrowed to at most gifts_amount_max gifts. in each iteration the threshold an thus the search area is reduced more based on the amount
+        # of gifts that are still left to an expected amount of gifts_amount_target. this means, if far too many gifts are found, the threshold is 
+        # reduced much more (calculation is based on an equal distribution, which is obviously not correct).
+
+        while len(gifts_not_assigned_yet.index) >= gifts_amount_max:
+            threshold_longitude = threshold_longitude * math.sqrt(gifts_amount_target / len(gifts_not_assigned_yet.index))
+            threshold_latitude = threshold_latitude * math.sqrt(gifts_amount_target / len(gifts_not_assigned_yet.index))
+
+            gifts_not_assigned_yet = gifts_not_assigned_yet.where(gifts_not_assigned_yet["Longitude"] >= (gift["Longitude"] - threshold_longitude))
+            gifts_not_assigned_yet = gifts_not_assigned_yet.where(gifts_not_assigned_yet["Longitude"] <= (gift["Longitude"] + threshold_longitude))
+            gifts_not_assigned_yet = gifts_not_assigned_yet.where(gifts_not_assigned_yet["Latitude"] >= (gift["Latitude"] - threshold_latitude))
+            gifts_not_assigned_yet = gifts_not_assigned_yet.where(gifts_not_assigned_yet["Latitude"] <= (gift["Latitude"] + threshold_latitude))
+            gifts_not_assigned_yet = gifts_not_assigned_yet.dropna()
         return gifts_not_assigned_yet
